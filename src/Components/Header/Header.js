3d14cdfb5d5fd7cc77781244b892e65240156a68
calher99 <calher99@gmail.com>
2023-01-12 10:54:05 +0000

    create Meals and Header sections

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,21 @@
-
+import React from 'react';
+import classes from './Header.module.css'
+import mealsPic from '../../assets/meals.jpg'
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = () => {
 
-
+    return (
+        <React.Fragment>
+            <header className={classes.header}>
+                <h1>ReactMeals</h1>
+                <HeaderCartButton/>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsPic} alt='Table with food'/>
+            </div>
+        </React.Fragment>
+    );
 }
 
 export default Header;
